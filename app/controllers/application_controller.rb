7b3e057650ad206807da70f14a03fba467e65f0a
Thang Nguyen <thang@tinypulse.com>
2018-05-12 10:30:08 +0700

    update controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  respond_to :json
+  
+  before_action :underscore_params!
+
+  def underscore_params!
+    params.deep_transform_keys!(&:underscore)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180511045313) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title",           limit: 255
+    t.string   "slug",            limit: 255
+    t.text     "body",            limit: 65535
+    t.string   "description",     limit: 255
+    t.boolean  "favoristed",                    default: false
+    t.integer  "favorites_count", limit: 4
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
+  end
 
 end
